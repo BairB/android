1864a5ca769241e844bdc298a00f12eb801786da
Bair <pinhaev17@gmail.com>
2019-09-23 16:08:14 +0800

    2

--- app/src/main/java/com/example/routescreen/MainActivity.java
+++ app/src/main/java/com/example/routescreen/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.routescreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button btnScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void addListenerActivity () {
+
     }
 }
