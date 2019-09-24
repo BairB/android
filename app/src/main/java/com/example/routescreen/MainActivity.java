0d549edff5bb12eb017d7f991538db27687fbe91
Bair <pinhaev17@gmail.com>
2019-09-24 14:39:44 +0800

    1

--- app/src/main/java/com/example/routescreen/MainActivity.java
+++ app/src/main/java/com/example/routescreen/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.routescreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,9 +13,9 @@ private Button btnScreen;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addListenerOnButton ();
     }
-    public void addListenerOnButton () {
-
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, TwoScreen.class);
+        startActivity(intent);
     }
 }
